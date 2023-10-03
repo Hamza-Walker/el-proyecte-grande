b680c6d497bfd8c3efe3b51fc1b43cdccf02fae8
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-10-03 09:25:30 +0200

    added target and idea to .gitignore file to instruct Git to ignore the target directory/idea and its contents. - added some comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
--- Create the TBL_USERS table
+-- Create the TBL_USERS table with schema
 CREATE TABLE TBL_USERS (
-                           id INT AUTO_INCREMENT PRIMARY KEY,
-                           first_name VARCHAR(250) NOT NULL,
-                           last_name VARCHAR(250) NOT NULL,
-                           email VARCHAR(250) DEFAULT NULL
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       first_name VARCHAR(250) NOT NULL,
+       last_name VARCHAR(250) NOT NULL,
+       email VARCHAR(250) DEFAULT NULL
 );
 
 -- Insert data into the TBL_USERS table
